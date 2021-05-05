7f2c31a48e661f4385ac06e0e69e26d61574bf04
ErickYepremyan <epremyan_erik@mail.ru>
2021-05-05 11:22:23 +0400

    Day8 for Mher

---
+++ src/Day8/dividor7.java
@@ -0,0 +1,13 @@
+package Day8;
+
+public class dividor7 {
+    public static void main(String[] args) {
+        int [] data = new int[1000];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (i+1)*7;
+        }
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i] + " ");
+        }
+    }
+}
---
+++ src/Day8/fibonacciReversed.java
@@ -0,0 +1,15 @@
+package Day8;
+
+public class fibonacciReversed {
+    public static void main(String[] args) {
+        int [] fibonacci = new int[10];
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+        for (int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+        }
+        for (int i = fibonacci.length-1; i >=0 ; i--) {
+            System.out.println(fibonacci[i]);
+        }
+    }
+}
